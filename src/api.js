6feb91d576c424788a9353789f0f5e74ac1329a1
srikanth nenavath <nenavath.srikanth29@gmail.com>
2022-11-11 16:08:18 -0500

    react weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0cbc4fa370msh976e99edfa703ebp138dbbjsne9752ecd9e11",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b36c1529d9ef880e9da50a1f089aa8fb";
